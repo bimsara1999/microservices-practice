caa9e967fecbd2d041544834ab86f1a1ec153fcb
bimsara1999 <blankanatha@gmail.com>
2024-08-22 00:35:42 +0545

    Cloud bus

---
+++ employee-service/src/main/java/com/springbootAcademy/employee_service/controller/MessageController.java
@@ -0,0 +1,16 @@
+package com.springbootAcademy.employee_service.controller;/*package com.springbootAcademy.department_service.controller;
+
+
+@RestController
+@RefreshScope // aluthen add karapuwa refresh wenna
+public class MessageController {
+
+    @Value({"spring.boot.message"})
+    private String message;
+
+
+    @GetMapping("/users/message")
+    public String Message(){
+    return message;
+    }
+*/
