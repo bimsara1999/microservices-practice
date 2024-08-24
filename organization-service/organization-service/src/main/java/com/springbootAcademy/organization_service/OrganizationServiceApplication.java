aff9328ad8c590d1921b25738320f8ed73ea8b10
bimsara1999 <blankanatha@gmail.com>
2024-08-24 14:58:34 +0545

    new organization upnew

--- organization-service/organization-service/src/main/java/com/springbootAcademy/organization_service/OrganizationServiceApplication.java
+++ organization-service/organization-service/src/main/java/com/springbootAcademy/organization_service/OrganizationServiceApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-// @EnableEurekaClient
+@EnableEurekaClient
 public class OrganizationServiceApplication {
 
 	public static void main(String[] args) {
